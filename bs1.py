e39324b5e6fda1de4e0edc13b99c2f5835f14518
Allwin-Smily <46564211+Allwin-Smily@users.noreply.github.com>
2019-01-31 19:52:56 -0800

    Update bs1.py

--- bs1.py
+++ bs1.py
@@ -1,7 +1,7 @@
 a=int(input("enter the number\n"))
 if a<0:
-  print("number is negative")
+	print("number is negative")
 elif a>0:
-  print("number is positive")
+	print("number is positive")
 else:
 	print("number is zero")
